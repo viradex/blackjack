952efc65068d249730cd9c54a35fa157a6468688
Arnav Thorat <arnavthorat78@gmail.com>
2026-02-27 19:20:39 +1100

    Add basic card and deck logic

--- logic/card.py
+++ logic/card.py
@@ -1,9 +1,19 @@
 class Card:
-    def __init__(self, suit, rank, value):
+    def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
-        self.value = value
+
+    @property
+    def value(self):
+        if self.rank in ("J", "Q", "K"):
+            return 10
+        elif self.rank == "A":
+            return 11  # decision on soft/hard is made later
+        else:
+            return int(self.rank)
+
+    def get_path(self):
+        return f"assets/{self.rank}/{self.rank}{self.suit}.png"
 
     def __repr__(self):
-        # For debugging
-        return f"{self.rank} of {self.suit} with value {self.value}"
+        return f"{self.rank}{self.suit}"
--- logic/deck.py
+++ logic/deck.py
@@ -1,18 +1,34 @@
 import random
-from .card import Card
+from card import Card
 
 
 class Deck:
     def __init__(self):
+        self.rebuild()
+
+    def build(self):
+        ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        suits = ["H", "D", "C", "S"]
+
+        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
+        return self.cards
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+        return self.cards
+
+    def deal(self):
+        return self.cards.pop()
+
+    def rebuild(self):
         self.cards = []
         self.build()
         self.shuffle()
 
-    def build(self):
-        pass
 
-    def shuffle(self):
-        pass
-
-    def deal(self):
-        pass
+if __name__ == "__main__":
+    deck = Deck()
+    print(deck.build())
+    print(deck.shuffle())
+    print(deck.deal())
+    print(len(deck.cards))
